c6155c1dea04e3f10414511662595a7904564a7a
Michael Caley <kneedeepwater@hotmail.com>
2023-10-17 17:28:23 +0100

    sort imports, set private false

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
