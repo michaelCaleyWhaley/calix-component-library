eecdfc892c5eb84539c7b3252c4c770c2e89c038
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2023-10-19 16:14:11 +0100

    test latest bump

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,7 @@
 export type NavInput = { value: string };
 
 function Nav({ value }: NavInput) {
-  return <h1 data-caley="true">{value}</h1>;
+  return <h1 data-caley="true">{value} test</h1>;
 }
 
 export { Nav };
